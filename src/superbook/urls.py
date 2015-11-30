6d03fafcdd4fadfca8fd8f79c409c0ff2df80fc7
Pragnesh Chaudhari <aarticianpc@gmail.com>
2015-11-30 15:41:51 +0530

    small change

--- src/superbook/urls.py
+++ src/superbook/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from accounts.views import SignInAndSignUp, LogoutView, ProductView, ServiceView, ContactView, AboutView
+from oscar.app import application
 
 urlpatterns = patterns(
     '',
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^$', SignInAndSignUp.as_view(template_name='home.html'),
         name='home'),
     url(r'^product/$', ProductView.as_view(),
@@ -17,4 +19,5 @@ urlpatterns = patterns(
     url(r'^accounts/logout$', LogoutView.as_view(),
         name='logout'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^oscar/', include(application.urls)),
 )
