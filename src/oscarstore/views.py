a3e7db7a9ff72789e7588e4de2940d0c6d2accc2
Canh Ho <canhhs91@gmail.com>
2015-12-04 15:28:09 +0000

    add old product version

--- src/oscarstore/views.py
+++ src/oscarstore/views.py
@@ -4,11 +4,15 @@ from oscar.core.loading import get_model
 
 Product = get_model('catalogue', 'Product')
 Category = get_model('catalogue', 'Category')
+ProductImage = get_model('catalogue', 'ProductImage')
 
 def main(request):
+    # print dir(ProductImage)
     products = Product.objects.all()
+
     product_rows = [[]];
     for product in products:
+        print product.primary_image()
         if(len(product_rows)==0 or len(product_rows[-1]) >=2):
             product_rows.append([])
         product_rows[-1].append(product);
